f9b8b77ef883ee350797b69031f7eb33cf81f19e
Prodis <prodis@gmail.com>
2019-09-13 20:40:49 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Miss Elixir
+
+Some functions that I miss in Elixir core.
+
+Miss Elixir library brings in a non-intrusive way some functions that, for different reasons, are
+not part of the Elixir core.
+
+## Installation
+
+The package can be installed by adding `miss_elixir` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:miss_elixir, "~> 0.1.0"}
+  ]
+end
+```
+
+## Usage
+
+TODO: List of functions
+
+
+## Full documentation
+
+The full documentation is available at [https://hexdocs.pm/miss_elixir](https://hexdocs.pm/miss_elixir).
+
+## Contributing
+
+See the [contributing guide](https://github.com/prodis/miss_elixir/blob/master/CONTRIBUTING.md).
+
+## License
+
+Miss Elixir is released under the Apache 2.0 License. See the [LICENSE](https://github.com/prodis/miss_elixir/blob/master/LICENSE) file.
+
+## Author
+
+[Fernando Hamasaki de Amorim (prodis)](https://github.com/prodis)
+
+![Prodis Logo](https://camo.githubusercontent.com/c01a3ebca1c000d7586a998bb07316c8cb784ce5/687474703a2f2f70726f6469732e6e65742e62722f696d616765732f70726f6469735f3135302e676966)
---
+++ mix.exs
@@ -0,0 +1,69 @@
+defmodule Miss.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/prodis/miss_elixir"
+
+  def project do
+    [
+      app: :miss_elixir,
+      name: "Miss Elixir",
+      version: @version,
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      dialyzer: dialyzer(),
+      description: description(),
+      package: package(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  def application, do: []
+
+  defp deps do
+    [
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.11.2", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    Some functions that I miss in Elixir core.
+    """
+  end
+
+  defp dialyzer do
+    [
+      ignore_warnings: "dialyzer.ignore"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Fernando Hamasaki de Amorim"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @github_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      canonical: "http://hexdocs.pm/miss_elixir"
+    ]
+  end
+end
